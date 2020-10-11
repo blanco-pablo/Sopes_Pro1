1b339ea62a2dc59ec0b3d42bd87babe340dd4acf
blanco-pablo <blancocalderonpablo@gmail.com>
2020-10-10 19:55:33 -0600

    C

--- servidorA/Modulos/cpu/cpu_grupo18.c
+++ servidorA/Modulos/cpu/cpu_grupo18.c
@@ -56,7 +56,7 @@ static int proc_llenar_archivo(struct seq_file *m, void *v) {
 
 	seq_printf(m, "TOTAL: %u\n",total);
 	seq_printf(m, "SEG: %u\n",segg);
-	num = (total / h ); // segg );
+	//num = (total / h ); // segg );
 	seq_printf(m, "1: %f\n", (total / h ));
 	return 0;
 }
