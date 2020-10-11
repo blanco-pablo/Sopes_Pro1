0267fdc28d032cd4de860d0fc80abb8bb5ab7701
blanco-pablo <blancocalderonpablo@gmail.com>
2020-10-10 20:58:50 -0600

    C

--- servidorA/API/app.py
+++ servidorA/API/app.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 def ram():
     with open('/proc/mem_grupo18') as f:
         for line in f:
-            a = line.to_dict()
-        return a["total"]
+            a = json.loads(line)
+        return a
     #with open('/proc/mem_grupo18') as f:
     #    data = json.load(f)
     #    print(data)
